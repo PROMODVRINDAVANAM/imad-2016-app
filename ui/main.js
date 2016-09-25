de302949b5d3c2ea787630ecae6f9a0ec73660a7
PROMODVRINDAVANAM <promodvrindavanam@gmail.com>
2016-09-25 14:43:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById("maintext");
 element.innerHTML = "Promod";
+// moving image
+var element = document.getElementById("madi");
+madi.onclick=function(){
+    madi.style.marginLeft="50px";
+};
 
