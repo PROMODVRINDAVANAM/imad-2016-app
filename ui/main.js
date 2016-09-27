7790bc3aa78c70a2bdf5173586e684b7f5d8f2e8
PROMODVRINDAVANAM <promodvrindavanam@gmail.com>
2016-09-27 19:58:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 //counter code
-var button=document.getElementById('count') ;
-var count=0;
+var button=document.getElementById('counter') ;
+var counter=0;
 button.onclick=function(){
-    counter=count+1;
+    counter=counert+1;
     var span=document.getElementById('count');
-    span.innerHTML=count.toString();
+    span.innerHTML=counter.toString();
 };
