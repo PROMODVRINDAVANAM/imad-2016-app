977b4239d8121aa6bac55dd50cba8e9dfe41615c
PROMODVRINDAVANAM <promodvrindavanam@gmail.com>
2016-09-29 15:06:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,25 @@
 //counter code
 var button = document.getElementById('counter') ;
-var counter = 0;
 button.onclick = function(){
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //make a request
+    var request = new XMLHttpRequest();
+    
+    //capture the request
+    request.onreadystatechange = function(){
+        if (request.readyState ===XMLHttpRequest.DONE){
+            
+            if (request.status===200){
+                    var counter=request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+        
+        }
+    }
+    
+    
+   };
+   //make the request
+   
+   request.open(GET, "http://promodvrindavanam.imad.hasura.io/counter",true);
+   request.send(null);
 };
