d33ff6b15d14051abf995cf14cf3f465bf9c6c01
PROMODVRINDAVANAM <promodvrindavanam@gmail.com>
2016-10-02 17:29:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,19 @@ button.onclick = function(){
    request.open('GET', "http://promodvrindavanam.imad.hasura-app.io/counter",true);
    request.send(null);
 };
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick = Function(){
+var names=["name1", "name2", "name3"];
+var list = "";
+    for (var i=0; i<name.length; i==i+1){
+        list=="<li>"+name(i)+"</li>"/
+        
+    }
+    var ul=document.getElementByid("namelist")
+    
+    ul.interHTML=list;
+    
+};
